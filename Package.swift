c4411bde548cb2a4a1a6494bd6244d606622aa70
Leo Dion <leogdion@brightdigit.com>
2024-10-08 22:18:11 -0400

    working on removing Swift-Log

--- Package.swift
+++ Package.swift
@@ -12,18 +12,9 @@ let package = Package(
       targets: ["FelinePine"]
     )
   ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
-  ],
   targets: [
     .target(
       name: "FelinePine",
-      dependencies: [
-        .product(
-          name: "Logging",
-          package: "swift-log"
-        )
-      ],
       swiftSettings: [
         SwiftSetting.enableUpcomingFeature("BareSlashRegexLiterals"),
         SwiftSetting.enableUpcomingFeature("ConciseMagicFile"),
